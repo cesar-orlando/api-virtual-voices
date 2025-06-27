99e2c6a6171f7b562e56859dbd98fe7db67dbfe2
Cesar Orlando Magaña Pasaye <cesarorlando@live.com.mx>
2025-06-27 08:29:08 -0600

    feat: cambios para render

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,4 @@
-export interface Request extends Express.Request {
-    // Add any custom properties to the request object here
-}
-
-export interface Response extends Express.Response {
-    // Add any custom properties to the response object here
-}
+// Remove custom Request/Response interfaces. Use express types directly in your code.
 
 // Exportar interfaces de tabla
 export { TableField, ITable } from '../models/table.model';
